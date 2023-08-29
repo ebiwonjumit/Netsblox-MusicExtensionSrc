2352e0320e092aa83da093716715bd3adf7e32df
Tito Ebiwonjumi <titoebiwonjumi@gmail.com>
2023-08-29 16:01:26 -0500

    Updated instrument location

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ import {WebAudioAPI} from "./WebAudioAPI/build/lib/webAudioAPI";
    const availableEffects = audioAPI.getAvailableEffects();
    audioAPI.getAvailableMidiDevices().then(returnMidiDevice, fail);
    audioAPI.getAvailableAudioInputDevices().then(returnAudioDevice, fail);
-   audioAPI.getAvailableInstruments('http://localhost:8000/extensions/MusicApp/instruments').then(
+
+   const devRoot = 'http://localhost:8000/extensions/BeatsBlox/instruments/';
+   const releaseRoot = 'https://extensions.netsblox.org/extensions/BeatsBlox/instruments/';
+   const instrumentLocation = window.origin.includes('localhost') ? devRoot : releaseRoot;
+
+   audioAPI.getAvailableInstruments(instrumentLocation).then(
        instruments => instruments.forEach(
            instrument => midiInstruments.push(instrument)
        )
