5c299509e2cd444d04b29646a973fbaba13449a0
Tito Ebiwonjumi <titoebiwonjumi@gmail.com>
2023-09-27 09:38:57 -0500

    Made name change to amp

--- src/index.js
+++ src/index.js
@@ -399,7 +399,7 @@ import {WebAudioAPI} from "./webAudioAPI";
                     }
                      },{ args: [], timeout: I32_MAX });
                  }),
-                 block('playNoteWithIntensity', 'command', 'music', 'play %noteDurations note(s) %s intensity %n', ['', 'C3', '75'], function (noteDuration, input, velocity){
+                 block('playNoteWithIntensity', 'command', 'music', 'play %noteDurations note(s) %s amp %n', ['', 'C3', '75'], function (noteDuration, input, velocity){
                     console.log(input);
                      this.runAsyncFn(async () =>{
                          const trackName = this.receiver.id;
